9323be0c5719aafadecc310eecb0969ad5d60b2e
unknown <PNTINTERN2@PNT-INTERN2.indiannica.net>
2022-07-01 15:33:23 +0530

    test file update

--- test.c
+++ test.c
@@ -1,16 +1,6 @@
 #include<stdio.h>
 void main()
 {
-    int i,n,fact=1;
-    printf("enter the factoral of a given no.");
-    scanf("%d\n",&n);
-    for(i=2;i<=n;i++)
-    {
-        fact=fact*i;
-    }
-    printf("\nfactorial of a %d is %d",n,fact);
-}hii
-<<<<<<< HEAD
-time complexity of fact is o(n-1)
-=======
->>>>>>> a2e6dee (hi added)
+    printf("hellow ji");
+}
+//snippets code test here
