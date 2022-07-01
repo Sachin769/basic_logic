1828caee47a2d7b02a67b777fd8ca8a4f2c39296
unknown <PNTINTERN2@PNT-INTERN2.indiannica.net>
2022-07-01 15:30:07 +0530

    use only for testing

---
+++ test.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i,n,fact=1;
+    printf("enter the factoral of a given no.");
+    scanf("%d\n",&n);
+    for(i=2;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    printf("\nfactorial of a %d is %d",n,fact);
+}hii
+<<<<<<< HEAD
+time complexity of fact is o(n-1)
+=======
+>>>>>>> a2e6dee (hi added)
