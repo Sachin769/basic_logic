ae8a2469efc1532c0473b7d02edeb93ee542a879
Sachin <pathaks411@gmail.com>
2022-07-04 14:47:09 +0530

    test2 updated

--- test2.c
+++ test2.c
@@ -1,2 +1,3 @@
 this is used for change the email address so that we can see the contribution
-vi ~/.gitconfig this is command to change the email addrress
+vi ~/.gitconfig this is command to change the email addrress.
+
