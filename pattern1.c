5a0f359ee690fec846a2f7d6d0d53974e9383274
Sachin <pathaks411@gmail.com>
2022-07-05 15:07:52 +0530

    O(n^2)

---
+++ pattern1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i>=j)
+            printf("*");
+            else
+            printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+//e.g. n=10
+//*
+//**
+//***
+//****
+//*****
+//******
+//*******
+//********
+//*********
+//**********
